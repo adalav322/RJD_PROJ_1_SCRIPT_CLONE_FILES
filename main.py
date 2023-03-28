4ba9d7665977ace5f67152e7d5a8bbcb8af80afe
IIIIIIIIIIIIIIIIIIII <adalav322@gmail.com>
2023-03-28 15:29:59 +0300

    3

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ for i in range(3):
     os.makedirs(f'outputFiles\Directory{i}')
 
 
+
 for rootdirO, dirsO, filesO, in os.walk(outpath):
     for dir in dirsO:
         for rootdir, dirs, files, in os.walk(inpath):
             for file in files:
-                shutil.copy(rootdir + '\\' + file,rootdirO + '\\' + dir)
+                if file in next(os.walk(rootdirO + '\\' + dir))[2]:
+                    pass
+                else:shutil.copy(rootdir + '\\' + file,rootdirO + '\\' + dir)
 
